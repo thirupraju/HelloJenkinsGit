d8be0ec92d0bf19eb1d195d10ea354f5775dc5f8
Thiruvengadam Perumal Raju <thiruve@sg.ibm.com>
2018-06-16 13:52:28 +0800

    third committed

--- HelloPrinter.java
+++ HelloPrinter.java
@@ -5,7 +5,7 @@ public class HelloPrinter {
 		// TODO Auto-generated method stub
 		System.out.println("--------------------------------");
 		System.out.println("**** Hello World to Jenkins ****");
-		System.out.println("**** Amended my  to Jenkins ****");
+		System.out.println("**** Amended and uploaded in git to Jenkins ****");
 		System.out.println("--------------------------------");
 	}
 
