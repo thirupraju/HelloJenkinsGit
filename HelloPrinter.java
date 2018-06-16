02fe9c6d94e97aed30458a90b03116e41cc896db
Thiruvengadam Perumal Raju <thiruve@sg.ibm.com>
2018-06-16 11:33:16 +0800

    first committed

---
+++ HelloPrinter.java
@@ -0,0 +1,11 @@
+
+public class HelloPrinter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("--------------------------------");
+		System.out.println("**** Hello World to Jenkins ****");
+		System.out.println("--------------------------------");
+	}
+
+}
